5efc636acf9068daa1b94cff6b0c2836b1d30870
Wellisson Lima <wellissonboanerges@gmail.com>
2026-05-07 17:12:34 -0300

    fix: correct vercel url typo in cors configuration

--- bom-sabor-delivery-web/src/services/api.js
+++ bom-sabor-delivery-web/src/services/api.js
@@ -1,9 +1,12 @@
 // src/services/api.js
 import axios from 'axios';
 
+const apiUrl = import.meta.env.VITE_API_URL;
+console.log("Conectando na API em:", apiUrl || "Localhost (Variável não encontrada)");
+
 const api = axios.create({
   // Se você mudou a porta no .env da API, ajuste aqui (ex: 5001)
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5001/api/',
+  baseURL: apiUrl || 'http://localhost:5001/api/',
 });
 
 export default api;
